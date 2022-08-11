029b746edf980556f22a15eb302a4bab35bb6c27
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-11 07:35:43 +0200

    Time: 614 ms (23.39%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
+#Method 1 using LIKE clause
+# SELECT patient_id, patient_name, conditions 
+# FROM Patients
+# WHERE conditions LIKE 'DIAB1%'
+# OR conditions LIKE '% DIAB1%';
+
+#Method 2 using REGEXP
 SELECT patient_id, patient_name, conditions 
 FROM Patients
-WHERE conditions LIKE 'DIAB1%'
-OR conditions LIKE '% DIAB1%';
+WHERE conditions REGEXP '^DIAB1| DIAB1' ;
