a8dfd94107940fdfa6037a62234274fd1a206f46
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-12 14:14:41 +0200

    Time: 196 ms (92.78%), Space: 0B (100.00%) - LeetHub

---
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH exp AS (SELECT DISTINCT(salary) FROM Employee ORDER BY salary DESC LIMIT 2)
+# SELECT COUNT(*) FROM exp;
+SELECT
+CASE 
+    WHEN 1 = (SELECT COUNT(salary) FROM exp) THEN NULL
+    ELSE (SELECT salary FROM exp ORDER BY salary ASC LIMIT 1)
+END SecondHighestSalary
+FROM exp LIMIT 1;
+    
