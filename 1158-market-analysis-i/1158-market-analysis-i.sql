2a7d9c6ca29a81f72ef04c2c6389d8745ed2bfa2
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-17 08:53:03 -0700

    Time: 1549 ms (45.62%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -14,4 +14,4 @@
 #better solution
 SELECT U.user_id buyer_id, U.join_date, count(O.order_id) orders_in_2019
 FROM Users U LEFT JOIN Orders O ON U.user_id = O.buyer_id AND YEAR(O.order_date)='2019'
-GROUP BY U.user_id
+GROUP BY U.user_id;
