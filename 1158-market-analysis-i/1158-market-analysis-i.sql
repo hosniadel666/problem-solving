3dd8ce79cb0d8807c4de0242cb74391b00f1ce83
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-17 08:52:50 -0700

    Time: 1097 ms (74.79%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,11 +1,17 @@
 # Write your MySQL query statement below
-WITH exp AS (
-    SELECT O.buyer_id, U.join_date, COUNT(*) orders_in_2019 
-    FROM Users U INNER JOIN Orders O ON U.user_id = O.buyer_id
-    WHERE O.order_date REGEXP '^2019'
-    GROUP BY O.buyer_id)
-SELECT * FROM exp
-UNION
-SELECT user_id, join_date, 0 orders_in_2019
-FROM Users 
-WHERE user_id NOT IN (SELECT buyer_id FROM exp);
+#1st solution
+# WITH exp AS (
+#     SELECT O.buyer_id, U.join_date, COUNT(*) orders_in_2019 
+#     FROM Users U INNER JOIN Orders O ON U.user_id = O.buyer_id
+#     WHERE O.order_date REGEXP '^2019'
+#     GROUP BY O.buyer_id)
+# SELECT * FROM exp
+# UNION
+# SELECT user_id, join_date, 0 orders_in_2019
+# FROM Users 
+# WHERE user_id NOT IN (SELECT buyer_id FROM exp);
+
+#better solution
+SELECT U.user_id buyer_id, U.join_date, count(O.order_id) orders_in_2019
+FROM Users U LEFT JOIN Orders O ON U.user_id = O.buyer_id AND YEAR(O.order_date)='2019'
+GROUP BY U.user_id
