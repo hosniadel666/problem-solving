252795cf30e68359203bc4ef4a8a1194cf158361
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-17 08:49:59 -0700

    Time: 1017 ms (85.31%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 WITH exp AS (
-    SELECT O.buyer_id, U.join_date, COUNT(*) orders_in_2019 FROM 
-    Users U INNER JOIN Orders O ON U.user_id = O.buyer_id
+    SELECT O.buyer_id, U.join_date, COUNT(*) orders_in_2019 
+    FROM Users U INNER JOIN Orders O ON U.user_id = O.buyer_id
     WHERE O.order_date REGEXP '^2019'
     GROUP BY O.buyer_id)
 SELECT * FROM exp
