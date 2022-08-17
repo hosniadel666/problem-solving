093fe7fdbd8f16e3eef8601a7955ca44bc4b2dd9
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-17 15:28:39 -0700

    Time: 17 ms (10.24%), Space: 7.8 MB (25.39%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -6,13 +6,10 @@ public:
         if(s.length() == 0)
             return false;
         s = string_trim(s);
-        cout << s;
-        
         for(int i = 0; i < s.length(); i++) {
             if(s[i] != s[s.length() - 1 - i])
                 return false;
         }
-        
         return true;
     }
     string string_trim(string s) {
