179e23697463cdd8e49c82cceb7a254fbb1d1325
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-18 04:24:20 -0700

    Time: 2819 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,9 +1,17 @@
 # Write your MySQL query statement below
-WITH exp AS(SELECT product_id FROM Sales 
-            WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'),
-     exp2 AS(SELECT product_id FROM Sales 
-            WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'),
-     exp3 AS (SELECT product_id FROM exp WHERE product_id 
-              NOT IN (SELECT * FROM exp2))
+# WITH exp AS(SELECT product_id FROM Sales 
+#             WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'),
+#      exp2 AS(SELECT product_id FROM Sales 
+#             WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'),
+#      exp3 AS (SELECT product_id FROM exp WHERE product_id 
+#               NOT IN (SELECT * FROM exp2))
+# SELECT product_id, product_name 
+# FROM Product WHERE product_id IN (SELECT * FROM exp3);
+
+#Better solution
+WITH exp AS (SELECT product_id FROM Sales 
+             GROUP BY product_id
+             HAVING MAX(sale_date) <= '2019-03-31'
+                    AND MIN(sale_date) >= '2019-01-01')
 SELECT product_id, product_name 
-FROM Product WHERE product_id IN (SELECT * FROM exp3);
+FROM Product WHERE product_id IN (SELECT * FROM exp);
