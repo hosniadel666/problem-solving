f3d8242de6d12277c144339b986cb92b30d7efab
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-18 04:17:34 -0700

    Time: 1400 ms (51.19%), Space: 0B (100.00%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+WITH exp AS(SELECT product_id FROM Sales 
+            WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'),
+     exp2 AS(SELECT product_id FROM Sales 
+            WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'),
+     exp3 AS (SELECT product_id FROM exp WHERE product_id 
+              NOT IN (SELECT * FROM exp2))
+SELECT product_id, product_name 
+FROM Product WHERE product_id IN (SELECT * FROM exp3);
