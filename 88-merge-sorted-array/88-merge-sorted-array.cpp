62b696cb57e3b4c53d146e6be32189c11dc877e3
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-20 08:17:27 -0700

    Time: 2 ms (77.83%), Space: 8.9 MB (99.29%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -15,10 +15,10 @@ public:
             nums1[m + n - 1] = nums2[n - 1];
             n--;
         }
-        while(m > 0) {
-            nums1[m + n - 1] = nums1[m - 1];
-            m--;
-        }
+        // while(m > 0) {
+        //     nums1[m + n - 1] = nums1[m - 1];
+        //     m--;
+        // }
 
     }
 };
