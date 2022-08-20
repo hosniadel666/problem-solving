5ef31229a4aebe1430d38441f44f4debd2aadac9
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-20 16:45:51 -0700

    Time: 10 ms (32.44%), Space: 9.8 MB (22.77%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return bst(nums, 0, nums.size() - 1, target);
+    }
+    int bst(vector<int>& nums, int start, int end, int target) {
+        int mid = (start + end) / 2;
+        if(target == nums[mid])
+            return mid;
+        else if(target > nums[mid])
+            return (end > mid)? bst(nums, mid + 1, end, target):end + 1;
+        else
+            return (start < mid)? bst(nums, start, mid - 1, target):start;
+        
+    }
+};
