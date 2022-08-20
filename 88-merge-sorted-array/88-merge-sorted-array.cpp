5ce25c09f609fa833903cc0b58d7405b0ab4849d
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-20 08:23:07 -0700

    Time: 6 ms (35.86%), Space: 8.9 MB (99.29%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -15,6 +15,7 @@ public:
             nums1[m + n - 1] = nums2[n - 1];
             n--;
         }
+        // we don't need to check the remanings of nums1, they are in the right places
         // while(m > 0) {
         //     nums1[m + n - 1] = nums1[m - 1];
         //     m--;
