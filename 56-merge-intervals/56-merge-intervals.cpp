b8ad644ae8951a617b0814e4c96c78e665768934
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-22 09:56:14 -0700

    Time: 163 ms (8.23%), Space: 46.9 MB (6.56%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -12,24 +12,28 @@ public:
         for(int i = 0; i < input.size(); i++) 
             output.push_back(input[i]);
         
-        
         return output;
     }
     void recurse(deque<vector<int>>& intervals, int index) {
+        
         if(index == intervals.size() - 1 || intervals.size() == 1)
             return;
+        
         vector<int> temp1 = intervals[index];
         vector<int> temp2 = intervals[index + 1];
 
         if(temp1[1] >= temp2[0]){
+            
             intervals.erase(intervals.begin() + index, intervals.begin() + index + 2);
+            
             if(temp1[1] < temp2[1]) 
                 temp1[1] = temp2[1];
+            
             intervals.insert(intervals.begin() + index, temp1);
             return recurse(intervals, index);
-        } else {
+        } else 
             return recurse(intervals, index + 1);
-        }
+        
             
     }
 };
