416ada52349541f18b2ff154e1e26c8467212b1e
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-23 09:38:06 -0700

    Time: 35 ms (44.42%), Space: 12.4 MB (6.34%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> m;
         vector<int> output;
         for(int i = 0; i < nums.size(); i++) {
-            for(int j = i + 1; j < nums.size(); j++) {
-                if( target - nums[i] == nums[j]) {
-                    output.push_back(i);
-                    output.push_back(j);
-                    return output;
-                }
-            }
+            if(m.find(nums[i]) != m.end()) {
+                output.push_back(m[nums[i]]);
+                output.push_back(i);
+            } else 
+                m[target - nums[i]] = i;
+                  
         }
         return output;
+        
+        
+        // THe complexity of this solution is O(n * n)
+        // vector<int> output;
+        // for(int i = 0; i < nums.size(); i++) {
+        //     for(int j = i + 1; j < nums.size(); j++) {
+        //         if( target - nums[i] == nums[j]) {
+        //             output.push_back(i);
+        //             output.push_back(j);
+        //             return output;
+        //         }
+        //     }
+        // }
+        // return output;
     }
 };
