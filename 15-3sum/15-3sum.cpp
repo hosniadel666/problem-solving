70f958c3e189153627af7be468f2c4f7b2e7429b
Hosni Adel <47048509+hosniadel666@users.noreply.github.com>
2022-08-23 10:59:37 -0700

    Time: 125 ms (66.36%), Space: 23.4 MB (33.13%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,7 +7,7 @@ public:
         for(int i = 0; i < nums.size(); i++) {
             
             if(nums[i] > 0)
-                continue;
+                return solutions;
             
             if(i == 0 || nums[i] > nums[i - 1]) {
                 int start = i + 1;
